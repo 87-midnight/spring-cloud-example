37e02f88e7415efba86463662f68a7d0a1b8d450
AnnieCattice <938517458@qq.com>
2019-04-07 02:50:25 +0800

    feign sample添加README，接口输入日志，为单应用多副本feign负载均衡提供识别

--- spring-cloud-feign-sample/user-provider/src/main/java/com/lcg/sample/web/UserController.java
+++ spring-cloud-feign-sample/user-provider/src/main/java/com/lcg/sample/web/UserController.java
@@ -1,15 +1,18 @@
 package com.lcg.sample.web;
 
 import com.alibaba.fastjson.JSONObject;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@Slf4j
 public class UserController {
 
     @GetMapping(value = "/get")
     public Object getInfo(@RequestParam String id){
+        log.info("I'm a female.{}",id);
         JSONObject data = new JSONObject();
         data.put("id",id);
         data.put("timestamp",System.currentTimeMillis());
